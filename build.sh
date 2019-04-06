3c50d10af314e12b485bb27bfd52ca36306f61a2
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-04-07 00:02:35 +0200

    running on current user

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-docker run -it --rm \
+#!/bin/sh
+
+DOCKER_IMAGE=maven:3.6-jdk-8
+
+docker run -it --rm -u "$(id -u)" \
 --name my-maven-project \
 -v "$(pwd)":/usr/src/mymaven \
--w /usr/src/mymaven maven:3.6-jdk-8 \
+-w /usr/src/mymaven \
+$DOCKER_IMAGE \
 mvn clean package jib:buildTar && cat target/jib-image.tar | docker image load
 
