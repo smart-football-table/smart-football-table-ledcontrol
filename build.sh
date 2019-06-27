acc63608e417603029d27740f25f6c72dbff9bb7
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-06-27 12:12:50 +0200

    do not use bash features

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 DOCKER_IMAGE=maven:3.6-jdk-8
-PRJ_NAME=$(basename $PWD)
+PRJ_NAME=`basename "$PWD"`
 
 docker run -it --rm -u "$(id -u)" \
--v "$(pwd)":/usr/src/$PRJ_NAME \
+-v "$PWD":/usr/src/$PRJ_NAME \
+-v "$HOME/.m2":/root/.m2 \
 -w /usr/src/$PRJ_NAME \
 $DOCKER_IMAGE \
-mvn clean package jib:buildTar && cat target/jib-image.tar | docker image load
+mvn clean package jib:buildTar && docker image load -i target/jib-image.tar
 
