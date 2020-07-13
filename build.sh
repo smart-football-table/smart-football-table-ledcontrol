89efcc1a346c08d12e305409ef79093744a26ae6
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-07-13 08:44:21 +0200

    Migrated from docker maven build to mvn wrapper

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-DOCKER_IMAGE=maven:3.6-jdk-8
-PRJ_NAME=`basename "$PWD"`
-
-docker run -it --rm -u "$(id -u)" \
--v "$PWD":/usr/src/$PRJ_NAME \
--v "$HOME/.m2":/root/.m2 \
--w /usr/src/$PRJ_NAME \
-$DOCKER_IMAGE \
-mvn clean package jib:buildTar && docker image load -i target/jib-image.tar
+`dirname "$0"`/mvnw clean package jib:buildTar && docker image load -i target/jib-image.tar
 
